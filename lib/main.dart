54caee11170d80d376cf13c473fa15c01c4fc360
samro123 <thsam.20it5@vku.udn.vn>
2024-04-03 16:25:37 +0700

    start animations

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:animations/animated_align.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(primaryColor: Colors.blue),
+      home: const MyHome(),
+    );
+  }
+}
+
+class MyHome extends StatefulWidget {
+  const MyHome({Key? key}) : super(key: key);
+
+  @override
+  State<MyHome> createState() => _MyHomeState();
+}
+
+class _MyHomeState extends State<MyHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Flutter Animations"),
+      ),
+      body: Center(
+        child: ListView(
+          physics: const BouncingScrollPhysics(),
+          children: [
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => const AnimatedAlignExample(),));
+                }, child: Text("Animated Aligin Example")),
+          ],
+        ),
+      ),
+    );
+  }
+}
