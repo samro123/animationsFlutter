ceec63640e3e57f0b6eef9be647026af692b200f
samro123 <thsam.20it5@vku.udn.vn>
2024-04-10 20:28:11 +0700

    Animation Postioned Directional Transition

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:animations/animation_fade_transition.dart';
+import 'package:animations/animated_positioned_directional_transition.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -39,7 +39,7 @@ class _MyHomeState extends State<MyHome> {
           children: [
             ElevatedButton(
                 onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => const AnimationFadeTransition(),));
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => const AnimatedPositionedDirectionalTransition(),));
                 }, child: Text("Animated Aligin Example")),
           ],
         ),
